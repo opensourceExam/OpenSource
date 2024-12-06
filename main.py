6990c71798552f4715ff9d1b2f13d687f13a078f
Park <ibox0925@gmail.com>
2024-12-06 15:55:58 +0900

    빼기 기능 추가

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main():
             if choice == 1:
                 print(f"결과: {x} + {y} = {x + y}")
             elif choice == 2:
-                print("결과:")
+                print(f"결과: {x} - {y} = {x - y}")
             elif choice == 3:
                 print("결과:")
             elif choice == 4:
